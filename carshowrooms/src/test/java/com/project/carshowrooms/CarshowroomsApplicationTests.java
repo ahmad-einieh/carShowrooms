185c118cf165e21e6ca6267700011a7af93575d0
Ahmad Einieh <66053897+ahmad-einieh@users.noreply.github.com>
2025-03-14 13:01:05 +0300

    first commit

---
+++ carshowrooms/src/test/java/com/project/carshowrooms/CarshowroomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.carshowrooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarshowroomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
